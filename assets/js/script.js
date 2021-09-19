c2427bf236d88ba163a1a3e28c7d92846b9c0e6d
knakna <kna@tut.by>
2021-09-19 23:54:20 +0300

    add content for section app

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@ function test(a, b) {
     return a + b
 }
 
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
-
+$('.btns').on('click', '.btn', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
 //# sourceMappingURL=script.js.map
