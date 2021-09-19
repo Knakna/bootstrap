673900d74cabe5fdfcc4620ca52d2e4ba6f9bc60
knakna <kna@tut.by>
2021-09-19 23:54:20 +0300

    add content for section app

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,3 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+$('.btns').on('click', '.btn', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
